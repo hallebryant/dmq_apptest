e9e231f9f2aac19484a678d9ce9de40c488a265c
hallebryant <hallebryant@gmail.com>
2025-12-12 18:39:13 -0500

    correcting query

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
-df = conn.query('SELECT * FROM Artists;', ttl="10m")
+df = conn.query('SELECT * FROM "Artists";', ttl="10m")
 
 # Print results.
 for row in df.itertuples():
