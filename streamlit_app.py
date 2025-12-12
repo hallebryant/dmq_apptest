a48f1cbfdc16f8a3df7eef127860f9c0ec1f2814
hallebryant <hallebryant@gmail.com>
2025-12-12 17:43:00 -0500

    streamlit init

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM Artists;', ttl="10m")
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.artist_id} is named :{row.artist_name}:")
